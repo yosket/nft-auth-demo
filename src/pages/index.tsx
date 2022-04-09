005bbc881ea96531b7de77d3d0c0b9ab43d7b4b5
Yosuke Tanaka <yosket87@gmail.com>
2022-04-10 01:24:58 +0900

    WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,46 @@
+import confetti from 'canvas-confetti'
 import { NextPage } from 'next'
+import Image from 'next/image'
 
 const HomePage: NextPage = () => {
-  return <div className=""></div>
+  const handleConfetti = () => {
+    confetti({
+      zIndex: 999,
+      particleCount: 100,
+      spread: 70,
+      origin: { x: 0.5, y: 0.75 },
+    })
+  }
+
+  return (
+    <div className="relative w-screen h-screen">
+      <Image src="/bg.jpg" layout="fill" objectFit="cover" alt="" />
+
+      <div className="absolute z-10 w-screen h-screen flex justify-center items-center p-8">
+        <div className="bg-white rounded-3xl shadow-2xl p-8 space-y-8">
+          <p className="text-5xl text-center">🔐</p>
+          <h1 className="text-3xl font-bold text-center">NFT Authentication</h1>
+          <p className="text-gray-400">
+            You can sign in if you have the following NFT
+          </p>
+          <div className="bg-gray-100 shadow-inner p-8 rounded-3xl flex items-center space-x-4">
+            <div className="rounded-full overflow-hidden w-10 h-10">
+              <Image src="/ens.jpeg" width={40} height={40} alt="" />
+            </div>
+            <p>Ethereum Name Service (ENS)</p>
+          </div>
+          <div className="flex justify-center">
+            <button
+              className="bg-blue-400 p-4 rounded-3xl text-white w-64 relative"
+              onClick={handleConfetti}
+            >
+              Connect Wallet
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
